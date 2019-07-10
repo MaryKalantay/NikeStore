3b7bc2eff1421df5048bfd34cb00f0ba5555ceec
MaryKalantay <marykalantay@MaryKalantay.local>
2019-07-10 10:14:06 +0300

    test localStorage

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 // click button "registr" 
 function Registration() {
-
-  let first_name = document.querySelector("#first_name").value;
-  let last_name = document.querySelector("#last_name").value;
-  let email = document.querySelector("#email").value;
-  let password_1 = document.querySelector("#password_1").value;
-  let password_2 = document.querySelector("#password_2").value;
-
   let new_user = {
     firstName: "No name", 
     lastName: "No name", 
@@ -15,6 +8,13 @@ function Registration() {
     password_1: "No name",
     dateLogin: new Date()
   };
+  console.log(new_user);
+
+  let first_name = document.querySelector("#first_name").value;
+  let last_name = document.querySelector("#last_name").value;
+  let email = document.querySelector("#email").value;
+  let password_1 = document.querySelector("#password_1").value;
+  let password_2 = document.querySelector("#password_2").value;
 
   new_user.firstName = first_name, 
   new_user.lastName = last_name, 
@@ -22,15 +22,17 @@ function Registration() {
   new_user.password_1 = password_1,
   new_user.password_1 = password_1;
 
-  localStorage.setItem (new_user.id, JSON.stringify(new_user));
-  
-  let userInlocalStorage = localStorage.getItem(new_user);
-  
-  console.log(new_user, userInlocalStorage);
+  localStorage.setItem ("user", JSON.stringify(new_user));
+  let userInlocalStorage = localStorage.getItem("user");
+
+  //дальше пробую делать сохранение и удаление сессии
+
+
+  console.log({new_user, userInlocalStorage});
+
 };
 
-document.getElementById("registr-submit").onclick = function() {Registration()};
-
+document.getElementById("registr-submit").addEventListener("click", Registration) 
 
 // test sessionStorage & localStorage
 if (window.sessionStorage && window.localStorage) {
@@ -39,8 +41,3 @@ if (window.sessionStorage && window.localStorage) {
 else {
   console.log("объекты sessionStorage и localtorage Не поддерживаются")
 }
-
-let colorRegistrBtn;
-localStorage.setItem("colorRegistrBtn","green");
-var bgColor = localStorage.getItem("colorRegistrBtn");
-console.log(bgColor);

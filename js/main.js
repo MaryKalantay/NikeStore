8e5c6d1efc0d6ae6172ff91178470fd7c59f8962
MaryKalantay <marykalantay@MaryKalantay.local>
2019-07-14 22:08:51 +0300

    Validation & disable btn

--- js/main.js
+++ js/main.js
@@ -10,36 +10,46 @@ let new_user = {
 console.log(new_user);
 
 // Validation
-document.querySelector(".form-input[name=first_name]").addEventListener("input", Validation);
-document.querySelector(".form-input[name=last_name]").addEventListener("input", Validation);
-document.querySelector(".form-input[name=email]").addEventListener("input", Validation);
-document.querySelector(".form-input[name=pass_1]").addEventListener("input", Validation);
-document.querySelector(".form-input[name=pass_2]").addEventListener("input", Validation);
-
 function Validation() {
-  if (this.value != " " ) {
-    document.getElementById("registr-submit").removeAttribute("disabled", "");
-    console.log("name is correct");
+  var first_name = document.querySelector(".form-input[name=first_name]").value;
+  var last_name = document.querySelector(".form-input[name=last_name]").value;
+  var email = document.querySelector(".form-input[name=email]").value;
+  var password_1 = document.querySelector(".form-input[name=pass_1]").value;
+  var password_2 = document.querySelector(".form-input[name=pass_2").value;
+
+  if (   (first_name != "") && (last_name != "") && (email != "") && (password_1 != "") && (password_2 != "")  ) {
+     if ( password_1 == password_2 ) {
+      document.getElementById("registr-submit").removeAttribute("disabled", "");
+      console.log("name is correct");
+     }
+     else {
+      document.getElementById("registr-submit").setAttribute("disabled", "disabled");
+    }
   }
 }
 
+document.querySelector(".form-input").addEventListener("input", Validation);
+document.addEventListener('input', Validation);
+
+// Registration
 function Registration() {
-  let first_name = document.querySelector(".form-input[name=first_name]").value;
-  let last_name = document.querySelector(".form-input[name=last_name]").value;
-  let email = document.querySelector(".form-input[name=email]").value;
-  let password_1 = document.querySelector(".form-input[name=pass_1]").value;
-  let password_2 = document.querySelector(".form-input[name=pass_2").value;
-  new_user.firstName = first_name, 
-  new_user.lastName = last_name, 
-  new_user. email = email, 
-  new_user.password_1 = password_1,
-  new_user.password_1 = password_1;
+  new_user.firstName  = document.querySelector(".form-input[name=first_name]").value, 
+  new_user.lastName  = document.querySelector(".form-input[name=last_name]").value, 
+  new_user. email = document.querySelector(".form-input[name=email]").value, 
+  new_user.password_1 = document.querySelector(".form-input[name=pass_1]").value,
+  new_user.password_1  = document.querySelector(".form-input[name=pass_2]").value;
   console.log({new_user});
 };
 
-// Action btn for registration
-function ActionRegistrButton() {
-  Registration();
-}
+function registerMessage() {
+  document.querySelector(".form-fields").style.display = "none";
+  document.querySelector("#registr-submit").style.display = "none"
+  document.querySelector(".registr-form").innerHTML += "Thank you <b>" + new_user.firstName + " " + new_user.lastName + "</b> for registration</h2>";
+} 
 
-document.getElementById("registr-submit").addEventListener("click", ActionRegistrButton) 
+// Action btn for registration
+document.getElementById("registr-submit").addEventListener("click", function(event) {
+  Registration();
+  registerMessage();
+  event.preventDefault();
+}, false);

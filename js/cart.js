a6ba9c313a4727bee0886c079cf254609adfeccb
MaryKalantay <marykalantay@MaryKalantay.local>
2019-07-26 09:48:09 +0300

    Parse JSON in cycle, and create html mockup in js

--- js/cart.js
+++ js/cart.js
@@ -4,7 +4,8 @@ var sectionShoes = document.querySelector(".catalog-popular-arrivals");
 // var section = document.querySelector('section');
 
 
-var requestURL = "js/goods.json";
+
+var requestURL = "goods.json";
 console.log(requestURL)
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
@@ -17,20 +18,49 @@ request.onload = function() {
   var arrShoes = request.response;
   console.log("Запрос:" + arrShoes);
 
-  popularArrivals(arrShoes);
-  // showHeroes(arrShoes);
+  var a = JSON.stringify(arrShoes);
+  console.log(a)
+  var b = JSON.parse(a);
+  console.log(b)
+
+  popularArrivals(b);
+   
 }
 
-function popularArrivals(jsonObj) {
-  var sectionShoes = document.querySelector(".catalog-popular-arrivals");
 
-    var nameItem = document.createElement("p");
+function popularArrivals(jsonObj) {
+  var nameItem = document.createElement("ul");
+  nameItem.classList = "popular-list";
+  var sectionShoes = document.querySelector(".popular-list");
+
+  const markup = `
+  <li class="popular-item">
+     <h2>
+         test
+     </h2>
+     <p class="location">test</p>
+     <p class="bio">test</p>
+  </div>
+ `;
+
+ sectionShoes.innerHTML = markup;
+
+  for (var i = 0; i < jsonObj.length; i++) {
+    
+    console.log(jsonObj[i]);
+
     console.log(nameItem);
-    nameItem.textContent = jsonObj["name"];
+    nameItem.textContent = jsonObj[i].name;
     sectionShoes.appendChild(nameItem);
 
     var priceItem = document.createElement('p');
     console.log(priceItem);
-    priceItem.textContent = '$: ' + jsonObj['price'] ;
+    priceItem.textContent = '$: ' +  jsonObj[i].price ;
     sectionShoes.appendChild(priceItem); 
+
+  }
+
+
+
+
 }

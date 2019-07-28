a14bf516dab63c9c4c66bcf095386f6339eb4d5f
MaryKalantay <marykalantay@MaryKalantay.local>
2019-07-28 12:12:03 +0300

    JSON parse catalog in template

--- js/cart.js
+++ js/cart.js
@@ -3,8 +3,6 @@
 var sectionShoes = document.querySelector(".catalog-popular-arrivals");
 // var section = document.querySelector('section');
 
-
-
 var requestURL = "goods.json";
 console.log(requestURL)
 var request = new XMLHttpRequest();
@@ -19,48 +17,37 @@ request.onload = function() {
   console.log("Запрос:" + arrShoes);
 
   var a = JSON.stringify(arrShoes);
-  console.log(a)
+  console.log(a);
   var b = JSON.parse(a);
-  console.log(b)
+  console.log(b);
 
   popularArrivals(b);
-   
 }
 
 
 function popularArrivals(jsonObj) {
-  var nameItem = document.createElement("ul");
-  nameItem.classList = "popular-list";
-  var sectionShoes = document.querySelector(".popular-list");
-
-  const markup = `
-  <li class="popular-item">
-     <h2>
-         test
-     </h2>
-     <p class="location">test</p>
-     <p class="bio">test</p>
-  </div>
- `;
-
- sectionShoes.innerHTML = markup;
-
-  for (var i = 0; i < jsonObj.length; i++) {
-    
-    console.log(jsonObj[i]);
-
-    console.log(nameItem);
-    nameItem.textContent = jsonObj[i].name;
-    sectionShoes.appendChild(nameItem);
-
-    var priceItem = document.createElement('p');
-    console.log(priceItem);
-    priceItem.textContent = '$: ' +  jsonObj[i].price ;
-    sectionShoes.appendChild(priceItem); 
-
+  var catalogShoes = document.querySelector(".popular-list");
+  for (key in jsonObj) {
+    console.log(jsonObj[key].name);
+    var item = document.createElement("li");
+    item.classList = "popular-item";
+    catalogShoes.appendChild(item);
+    var markup = `
+      <picture>
+        <img src="${jsonObj[key].img}" alt="Popular Arrivals">
+        <div class="overlay">
+          <a class="btn btn-overlay" href="#">
+            View Details
+          </a>
+        </div>
+      </picture>
+      <div class="popular-info">
+        <div class="name"><a href="#">${jsonObj[key].name}</a> <span class="outdoors">Shop Outdoors</span></div>
+        <div class="price">$ ${jsonObj[key].price}
+          <a href="" class="add-cart"><svg width="25" height="27"><use  xlink:href="#cart" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg></a>                  
+        </div>
+      </div>
+    `;
+    item.innerHTML = markup;
   }
-
-
-
-
 }

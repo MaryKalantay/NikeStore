c6da94e24243dc670e5818e4f0248a29c411ed64
MaryKalantay <marykalantay@MaryKalantay.local>
2019-07-28 22:12:21 +0300

    Not work add to cart next item

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,5 @@
 // User Cart
-
+var cart = {};
 var sectionShoes = document.querySelector(".catalog-popular-arrivals");
 // var section = document.querySelector('section');
 
@@ -20,34 +20,43 @@ request.onload = function() {
   console.log(a);
   var b = JSON.parse(a);
   console.log(b);
-
   popularArrivals(b);
 }
-
 
 function popularArrivals(jsonObj) {
   var catalogShoes = document.querySelector(".popular-list");
   for (key in jsonObj) {
-    console.log(jsonObj[key].name);
     var item = document.createElement("li");
     item.classList = "popular-item";
     catalogShoes.appendChild(item);
+
     var markup = `
       <picture>
         <img src="${jsonObj[key].img}" alt="Popular Arrivals">
         <div class="overlay">
-          <a class="btn btn-overlay" href="#">
+          <a class="btn btn-overlay" href="${jsonObj[key].link}">
             View Details
           </a>
         </div>
       </picture>
       <div class="popular-info">
-        <div class="name"><a href="#">${jsonObj[key].name}</a> <span class="outdoors">Shop Outdoors</span></div>
+        <div class="name"><a href="${jsonObj[key].link}">${jsonObj[key].name}</a> <span class="outdoors">Shop Outdoors</span></div>
         <div class="price">$ ${jsonObj[key].price}
-          <a href="" class="add-cart"><svg width="25" height="27"><use  xlink:href="#cart" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg></a>                  
+          <button data-item="${key}"  class="add-cart"><svg width="25" height="27"><use  xlink:href="#cart" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg></button>                  
         </div>
       </div>
     `;
     item.innerHTML = markup;
+    document.querySelector(".add-cart").addEventListener("click", addToCart);
   }
 }
+
+function addToCart() {
+  alert("+");
+    var dataItem = this.getAttribute("data-item");
+    cart[dataItem] = 1;
+    console.log(cart)
+  
+}
+
+
